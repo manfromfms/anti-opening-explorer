b58ed8ebea76f2fef69e225633fd48ecde2c1326
normchell_pc <nashvlad370@gmail.com>
2023-12-23 15:11:14 +0500

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.use((req, res, next) => {
+    console.log('Full Path:', req.originalUrl)
+
+    res.sendFile(__dirname + '/node_modules/chessgroundx' + req.originalUrl)
+});
